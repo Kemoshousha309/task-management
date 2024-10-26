2337e3412adba6da0375e89bc6c88574c97c32bd
kareem shousha <kemoshousha309@gmail.com>
2024-10-26 04:32:51 +0300

    add loading spinner

---
+++ src/Components/Spinner.tsx
@@ -0,0 +1,7 @@
+const Spinner = () => {
+  return (
+    <div className="w-16 h-16 border-4 border-t-transparent border-b-transparent border-l-transparent border-r-blue-500 rounded-full animate-spin"></div>
+  );
+};
+
+export default Spinner;
